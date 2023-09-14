2db65e1b08710d9bcb424ac11a9063e12a8e7b1d
kosw <swko4512@gmail.com>
2023-09-14 11:43:48 +0900

    2주차 이론

--- [CP2_02_이론].py
+++ [CP2_02_이론].py
@@ -2,15 +2,29 @@
 #     for e in L1:
 #         if e in L2:
 #             L1.remove(e)
+#
+# def remove_dups(L1, L2):
+#     L1_copy = L1[:]
+#     for e in L1_copy:
+#         if e in L2:
+#             L1.remove(e)
+#
+#
+# L1 = [1,2,3,4]
+# L2 = [1,2,5,6]
+# remove_dups(L1, L2)
+# print(L1)
 
-def remove_dups(L1, L2):
-    L1_copy = L1[:]
-    for e in L1_copy:
-        if e in L2:
-            L1.remove(e)
+def lyrics_to_frequencies (lyrics):
+    myDict = {}
+    for word in lyrics:
+        if word in myDict:
+            myDict[word] += 1
+        else:
+            myDict[word] = 1
+    return myDict
 
-
-L1 = [1,2,3,4]
-L2 = [1,2,5,6]
-remove_dups(L1, L2)
-print(L1)
+s = "row row row your boat"
+l = s.split(" ")
+d = lyrics_to_frequencies(l)
+print(d)
