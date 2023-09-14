000599e0b1e98cab23af98df195e57f78a92b398
kosw <swko4512@gmail.com>
2023-09-14 09:39:06 +0900

    test

--- [CP2_02].py
+++ [CP2_02].py
@@ -19,3 +19,20 @@ a = print_func_a()
 param = print_parameter(5)
 print(a)
 print(param)
+
+# def print_func_a():
+#     print("Printing func_a")
+# def print_parameter(x):
+#     print("Parameter x : ", x)
+#     return x
+# def exec_param_func(function):
+#     print("I try execute param function...")
+#     return function()
+#
+#
+#
+# print("Execute exec_param_func")
+# param_a = exec_param_func(print_func_a())
+# print(param_a)
+#
+# param_parameter = exec_param_func(print_parameter(5))
