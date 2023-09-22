a347b378c9e0b5b72bd9a96bbba381815b08fa26
kosw <swko4512@gmail.com>
2023-09-22 10:50:44 +0900

    [ADD : 3주차 실습 과제 2]

---
+++ [CP2_03-2].py
@@ -0,0 +1,13 @@
+print("Menu")
+menu = {
+    'Pasta': 6500,
+    'Pilau': 7000,
+    'Kimbap': 2500,
+    'Albab': 5300,
+    'Pork Cutlet': 5500,
+    'Jajangmyeon': 6500,
+    'Kimchijjigae': 5800
+}
+
+for i,j in menu.items():
+    print(i + " :  " + str(j) + " Won")
