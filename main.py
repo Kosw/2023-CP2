d0e148bb2f7470c83aa3164c63de016528b99ffc
kosw <swko4512@gmail.com>
2023-10-21 09:22:16 +0900

    [ADD] 과제

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-# 최대 경로 합을 찾는 함수
+#최대 경로 합
 def dogfood(matrix):
     if not matrix:
         return 0
     rows, cols = (len(matrix), len(matrix[0]))
     dp = [[0] * cols for _ in range(rows)]
 
-    # 첫 번째 행은 그대로 복사
+    #1행 복사
     for j in range(cols):
         dp[0][j] = matrix[0][j]
 
-    # 각 셀마다 최대 경로 합 계산
+    #최대 경로 합 계산
     for i in range(1, rows):
         for j in range(cols):
             dp[i][j] = matrix[i][j] + max(dp[i - 1][j], dp[i][max(0, j - 1)])
     return dp
 
 
-# 사용자로부터 행렬을 입력받는 함수
+#행렬 입력
 def get():
     m = []
     row, col = map(int, input().split())
@@ -25,7 +25,7 @@ def get():
     return m
 
 
-# 최대 경로를 찾는 과정에서 거치는 좌표들을 계산하는 함수
+#좌표 계산
 def findfood(Sum, rr, cc):
     cnt = []
     j, k = rr - 1, cc - 1
@@ -47,15 +47,14 @@ def findfood(Sum, rr, cc):
     return cnt[::-1]
 
 
-# 행렬 입력 받기
+#행렬 입력
 matrix = get()
 
-# 최대 경로 합 계산
+#경로 합
 result = dogfood(matrix)
 
-# 최대 경로의 좌표 계산
+#좌표
 path = findfood(result, len(matrix), len(matrix[0]))
 
-# 결과 출력
 for coord in path:
     print(coord[::-1])
